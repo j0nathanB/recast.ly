1a85ae5b742e3767d7d1715ec8c48708d539dd74
Hack Reactor Student <student@hackreactor.com>
2017-04-12 21:16:46 -0700

    Create searchYouTube helper function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,13 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = function searchYouTube(options, callback) {
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    type: 'GET',
+    part: 'snippet',
+    key: options.key,
+    maxResults: options.max,
+    q: options.query,
+    videoEmbeddable: "true",
+  }, callback );
+
 };
 
 window.searchYouTube = searchYouTube;
