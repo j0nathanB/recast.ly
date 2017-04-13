92225f77e08d798c58974fcc1955e2ea160e252a
Hack Reactor Student <student@hackreactor.com>
2017-04-13 12:36:57 -0700

    Pass last searchYoutTube test

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,13 +1,19 @@
-var searchYouTube = function searchYouTube(options, callback) {
-  $.get('https://www.googleapis.com/youtube/v3/search', {
-    type: 'GET',
-    part: 'snippet',
+var searchYouTube = function(options, cb) {
+  $.get('https://www.googleapis.com/youtube/v3/search', 
+  {
+  	part: 'snippet',
     key: options.key,
     maxResults: options.max,
     q: options.query,
-    videoEmbeddable: "true",
-  }, callback );
-
-};
-
+    type:'video',
+    videoEmbeddable: 'true'
+  }) 
+  	.done(function(data) {
+  		cb(data.items);
+  		console.log('good', data);
+  	})
+	.fail(function(data){
+		console.log('error', data);
+	});
+}
 window.searchYouTube = searchYouTube;
